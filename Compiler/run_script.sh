e8b0d07e97237e708cbe684df6cb2dcfd5283c83
Siddharth Santurkar <siddharth.santurkar@ieee.org>
2013-09-30 01:26:49 +0530

    Basic compiler features; Execution in terminale window enabled;

--- Compiler/run_script.sh
+++ Compiler/run_script.sh
@@ -1,15 +1,32 @@
 #/bin/bash
-obj_file=$1
 cont=1
-echo "Started"
+i=0
+args_file=$1
+
+while read line; do
+	var[$i]=$line
+	i=$(( i + 1 ))
+done < $args_file
+
+echo ${var[2]}
+
+case ${var[1]} in
+	"c") "${var[0]}" "${var[2]}" ; ret=$? ;;
+	"cpp") "${var[0]}" "${var[2]}" ; ret=$? ;;
+	"java") echo "Execution of Java programs is currently unsupported" ;;
+	"py") python "${var[0]}" "${var[2]}"; ret=$? ;;
+	"sh") sh "${var[0]}" "${var[2]}"; ret=$? ;;
+	"rb") ruby "${var[0]}" "${var[2]}"; ret=$? ;;
+	"R") R CMD BATCH "${var[0]}" "${var[2]}"; ret=$? ;;
+	
+esac
+echo "-----------------------------------------------------------------"
+echo "Program exited with return code : $ret"
 echo "Press ENTER to exit"
-while [ $cont -eq "1" ]
-do
-read -s -n 1 in
-if [ "$in" = "" ];
-then
-cont=0
-exit
-fi
+while [ $cont -eq "1" ]; do
+	read -s -n 1 in
+	if [ "$in" = "" ]; then
+		cont=0
+	fi
 done
 	
